a1567038607c0d0a06a26c2cfda261753c5c0652
Prashant Sinha <prashant@ducic.ac.in>
2016-08-04 13:35:35 +0530

    used file descripter for jsons

--- vatika/treebank/migrate.py
+++ vatika/treebank/migrate.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from vatika.treebank.model import Sentence
 
 if __name__ == '__main__':
-  data = json.loads(sys.stdin.read())
+  data = json.load(sys.stdin, strict=False)
 
   for s in tqdm(data):
     ob = Sentence(parse_tree=s)
